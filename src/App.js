5f668d11cee70dfc334299accefedc3734e1f669
Fernando Fragoso <fernandofragoso@gmail.com>
2018-08-31 16:48:31 -0300

    Enzyme setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import Home from './components/home/Home';
+import Restaurants from './components/restaurants/Restaurants'
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      city: null,
+      filters: {
+        stars: [],
+        costs: [],
+        types: []
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>restaurant-guide</h1>
+        {this.state.city ? 
+          <Restaurants city={this.state.city} filters={this.state.filters} /> :
+          <Home />}
       </div>
     );
   }
---
+++ src/components/restaurants/Restaurants.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Restaurants.css';
+
+class Restaurants extends Component {
+  render() {
+    return (
+      <div className="Restaurants">
+        <h1>Restaurants</h1>
+      </div>
+    );
+  }
+}
+
+export default Restaurants;
