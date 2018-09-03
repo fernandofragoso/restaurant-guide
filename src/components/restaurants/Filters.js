b46af4f8aa8289f26e5770f5b2c9a937a641c48a
Fernando Fragoso <fernandofragoso@gmail.com>
2018-09-02 23:49:18 -0300

    Responsiveness

--- src/components/restaurants/Filters.js
+++ src/components/restaurants/Filters.js
@@ -39,10 +39,10 @@ class Filter extends Component {
         <span className="Filter__title">CUSTO PARA DUAS PESSOAS</span>
         <section className="Filter__section">
           <ul>
-            <li><input name="1" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> Até R$50</li>
-            <li><input name="2" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> De R$50 a R$80</li>
-            <li><input name="3" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> De R$80 a R$110</li>
-            <li><input name="4" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> Acima de R$110</li>
+            <li><input name="1" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> Até $50</li>
+            <li><input name="2" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> De $50 a $80</li>
+            <li><input name="3" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> De $80 a $110</li>
+            <li><input name="4" onChange={(e) => this.handleChange(e, 'costs')} type="checkbox"/> Acima de $110</li>
           </ul>
         </section>
         <span className="Filter__title">TIPO DE COZINHA</span>
